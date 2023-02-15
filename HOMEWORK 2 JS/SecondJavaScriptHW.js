ccaf825efd0d76e985d267c2afb0c6dbf3185e5e
Berat2111 <beratnuhi91@gmail.com>
2023-02-15 16:32:36 +0100

    SECONDJSHW

---
+++ HOMEWORK 2 JS/SecondJavaScriptHW.js
@@ -0,0 +1,17 @@
+let birthday = 21;
+let fullName = 9;
+let birthYear = 99;
+let yearSum = 1 + 9 + 9 + 9;
+
+let firstCalc = birthday > fullName;
+let secondCalc = birthday % yearSum;
+
+console.log(firstCalc);
+
+let thirdCalc = " jas sum ";
+
+if (firstCalc === true && secondCalc < 3) {
+    console.log(thirdCalc = "svetlina");
+} else {
+    console.log(thirdCalc + "temnica");
+}
