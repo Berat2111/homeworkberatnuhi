57ac70abb43f2b86f11f8a8301187ada1ff08d8d
Berat2111 <beratnuhi91@gmail.com>
2023-02-15 16:32:05 +0100

    FIRSTJSHW

---
+++ HOMEWORK 1 JS/FirstJavaScriptHW.js
@@ -0,0 +1,24 @@
+let name = "Berat Nuhi";
+let age = 20;
+let job = "Warehouse clerk"
+let gender = "male"
+let livesInSkopje = true;
+
+
+let person = {
+    name:"Berat Nuhi",
+    age: "20",
+    job: "Warehouse clerk",
+    gender: "male",
+    liveInSkopje: true,
+    
+
+}
+console.log(person);
+
+alert(person.name + " " + person.age + " "  + person.job + " " + person.gender + " " + person.liveInSkopje)
+
+function person1(){
+    alert(person.name + " " + person.age + " "  + person.job + " " + person.gender + " " + person.liveInSkopje);
+    
+}
